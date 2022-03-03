5b4b328a2d549fed758cd3df3010e60b30887a71
whh <wenhaoh5@nycstudents.net>
2022-03-03 13:17:07 -0500

    2nd commit

--- src/FunWith2DArrays.java
+++ src/FunWith2DArrays.java
@@ -10,4 +10,39 @@ public class FunWith2DArrays {
         System.out.println(lists[lists.length-1][0]);
         System.out.println(lists[lists.length-1][lists[lists.length-1].length-1]);
     }
+    public static double average(int [][] ints){
+        int rows = ints[0].length;
+        int columns = ints.length;
+        int total = 0;
+        int amount = 0;
+        for(int i = 0; i < rows; i ++)
+        {
+           for(int z = 0; z < columns; z++)
+           {
+               total += ints[z][i];
+               amount ++;
+           }
+        }
+        return (total * 1.0)/(amount);
+    }
+    public static int[] indexFound(String [][] str, String target){
+        int rows = str[0].length;
+        int columns = str.length;
+        int returnRows = -1;
+        int returnColumns = -1;
+
+        for(int i = 0; i < rows; i ++)
+        {
+            for(int z = 0; z < columns; z++)
+            {
+                if(str[z][i] == target)
+                {
+                    returnRows = z;
+                    returnColumns = i;
+                }
+            }
+        }
+        int[] falseReturn = {returnRows, returnColumns};
+        return falseReturn;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -59,6 +59,33 @@ public class Main {
             System.out.println(Arrays.toString(str));
         }
         System.out.println(FunWith2DArrays.totalElements(list3));
+
+        // TEST CODE FOR average
+        int[][] testArr1 = {{1, 2, 3}, {4, 5, 6}};
+        System.out.println(FunWith2DArrays.average(testArr1));
+        int[][] testArr2 = {{1, 2, 3, 4}, {7, 8, 9, 10}};
+        System.out.println(FunWith2DArrays.average(testArr2));
+        int[][] testArr3 = {{4, 5, 2}, {1, 9, 7}, {8, 10, 13}};
+        System.out.println(FunWith2DArrays.average(testArr3));
+        int[][] testArr4 = {{4}, {7}, {8}, {2}};
+        System.out.println(FunWith2DArrays.average(testArr4));
+        int[][] testArr5 = {{4}};
+        System.out.println(FunWith2DArrays.average(testArr5));
+
+        // TEST CODE FOR indexFound
+        String[][] words = {{"hi", "bye", "stuff", "go"}, {"time", "up", "you", "good"}, {"map", "low", "bow", "mom"}};
+        System.out.println(Arrays.toString(FunWith2DArrays.indexFound(words, "stuff")));
+        System.out.println(Arrays.toString(FunWith2DArrays.indexFound(words, "good")));
+        System.out.println(Arrays.toString(FunWith2DArrays.indexFound(words, "map")));
+        System.out.println(Arrays.toString(FunWith2DArrays.indexFound(words, "cat")));
+        System.out.println("--------------");
+
+        String[][] words2 = {{"time", "up", "bye"}, {"hi", "hit", "up"}, {"map", "bam", "low"}, {"bow", "mom", "joy"}};
+        System.out.println(Arrays.toString(FunWith2DArrays.indexFound(words2, "time")));
+        System.out.println(Arrays.toString(FunWith2DArrays.indexFound(words2, "joy")));
+        System.out.println(Arrays.toString(FunWith2DArrays.indexFound(words2, "bam")));
+        System.out.println(Arrays.toString(FunWith2DArrays.indexFound(words2, "cat")));
+
     }
 
 
